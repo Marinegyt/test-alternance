bf4afce2d650c626a05a666e259702adedf02cd7
Marine Gayet <marinegayet@gmail.com>
2024-07-30 10:24:00 +0200

    add service api call

---
+++ app/services/annonce_service.rb
@@ -0,0 +1,24 @@
+require 'faraday'
+
+class AnnonceService
+  def self.call_annonce
+    url = 'https://labonnealternance-recette.apprentissage.beta.gouv.fr/api/v1/jobs?romes=D1101&caller=contact%40domaine%20nom_de_societe&latitude=48.1113387&longitude=-1.6800198&radius=50&insee=35238'
+    params = nil
+    headrers = {'Content-Type' => 'application/json' 'charset=utf-8'}
+    response = Faraday.get(url, params, headers)
+    parsed_response = JSON.parse(response.body)
+    parsed_response['results'][1..-1].each do |data|
+      Annonce.create(
+        annonceId: data['id'],
+        title: data['title'],
+        description: data['job']['description'],
+        distance: data['place']['distance'],
+        fullAddress: data['place']['fullAddress'],
+        url: data['url'],
+        creationDate: data['job']['creationDate'],
+        contractType: data['job']['contactType'],
+        contractDescription: data ['job']['contractDescription'],
+      )
+    end
+  end
+end
